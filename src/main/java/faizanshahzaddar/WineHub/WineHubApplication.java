64aa8858539b13cd325cd58c4b1e55339b0399ef
faaizaan <fsdar3108@gmail.com>
2026-05-06 13:33:08 +0200

    Initial commit

---
+++ src/main/java/faizanshahzaddar/WineHub/WineHubApplication.java
@@ -0,0 +1,13 @@
+package faizanshahzaddar.WineHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/faizanshahzaddar/WineHub/WineHubApplicationTests.java
@@ -0,0 +1,13 @@
+package faizanshahzaddar.WineHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
