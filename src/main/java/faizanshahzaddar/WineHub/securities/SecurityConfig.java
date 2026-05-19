496a904aa83de8cb8c6b31e294103ec4c77e1d24
faaizaan <fsdar3108@gmail.com>
2026-05-19 13:54:51 +0200

    .

--- src/main/java/faizanshahzaddar/WineHub/securities/SecurityConfig.java
+++ src/main/java/faizanshahzaddar/WineHub/securities/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -43,6 +44,7 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
         );
 
+        httpSecurity.addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class);
         httpSecurity.cors(Customizer.withDefaults());
         return httpSecurity.build();
     }
